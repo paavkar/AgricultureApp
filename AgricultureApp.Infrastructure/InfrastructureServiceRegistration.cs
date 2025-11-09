dd1d5e0c1224052e8fc313bfb00d22d9d301e253
Paavo Karppinen <paavo_karppinen@outlook.com>
2025-11-09 23:29:53 +0200

    add HybridCache registration

--- AgricultureApp.Infrastructure/InfrastructureServiceRegistration.cs
+++ AgricultureApp.Infrastructure/InfrastructureServiceRegistration.cs
@@ -11,6 +11,15 @@ namespace AgricultureApp.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddHybridCache(options =>
+            {
+                options.DefaultEntryOptions = new()
+                {
+                    Expiration = TimeSpan.FromMinutes(30),
+                    LocalCacheExpiration = TimeSpan.FromMinutes(30)
+                };
+            });
+
             var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             services.AddDbContext<AgricultureAppDbContext>(options =>
                 options.UseSqlServer(connectionString));
