1fa90ee94092ea890997382ca0fbdbc57d871fff
Paavo Karppinen <paavo_karppinen@outlook.com>
2025-11-12 17:16:06 +0200

    add migration for initial user accounts

--- AgricultureApp.Infrastructure/Persistence/AgricultureAppDbContext.cs
+++ AgricultureApp.Infrastructure/Persistence/AgricultureAppDbContext.cs
@@ -1,16 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AgricultureApp.Domain.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgricultureApp.Infrastructure.Persistence
 {
-    public class AgricultureAppDbContext : DbContext
+    public class AgricultureAppDbContext(DbContextOptions<AgricultureAppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public AgricultureAppDbContext(DbContextOptions<AgricultureAppDbContext> options) : base(options)
-        {
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+        protected override void OnModelCreating(ModelBuilder builder) => base.OnModelCreating(builder);
     }
 }
